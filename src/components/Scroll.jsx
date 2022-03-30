eb59ebae67d825d71fe22296226c693901a0422e
ChernyshevaNetology <chernyshevak87@mail.ru>
2022-03-30 09:55:31 +0200

    updated readme page

--- src/components/Scroll.jsx
+++ src/components/Scroll.jsx
@@ -1,14 +1,6 @@
 import React, { useEffect, useState, useMemo, useCallback } from "react";
 import throttle from "lodash/throttle";
 
-// имитация запроса к серверу. просто получаем число асинхронно
-/*
-Есть реализация компонента, от которого требуется 2 вещи:
-    1) выводить текущее значение вертикального скролла окна (window.scrollY)
-    2) после монтирования асинхронно получить число и вывести его
-Нужно найти, объяснить и исправить как можно больше проблем в реализации
-*/
-
 const Scroll = () => {
   const fetchRandomNumber = useMemo(() => Promise.resolve(Math.random()), []);
 
